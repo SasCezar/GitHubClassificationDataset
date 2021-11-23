2930883ca9c9a45320167fa928a3ac393bf08e02
Cezar Sas <cezar.sas@gmail.com>
2021-11-23 14:20:19 +0100

    Fixed linking of an entity

--- src/ghcdio/deduplicate.py
+++ src/ghcdio/deduplicate.py
@@ -7,14 +7,14 @@ def duplicate2json(path):
 
     df = pd.read_csv(path)
 
-    df.drop(['map'], axis=1, inplace=True)
+    #df.drop(['map'], axis=1, inplace=True)
     df2 = df.copy(deep=True)
     df2.set_index(['topic'], drop=True, inplace=True)
     confidence = df2['confidence_score'].to_dict()
     df.drop(['confidence_score'], axis=1, inplace=True)
 
     res = df.groupby('Cluster ID')['topic'].apply(list).to_dict()
-    with open('/src/run/csv_example_output_list.json', 'wt') as outf:
+    with open('csv_example_output_list.json', 'wt') as outf:
         for k in res:
             topics = sorted([(x, confidence[x]) for x in res[k]], key=lambda x: x[1], reverse=False)
             r = {'cluster': k, 'topics': topics}
