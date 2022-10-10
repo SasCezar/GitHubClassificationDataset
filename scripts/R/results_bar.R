dac15c57181708e5fa99de9a6bf662b9ea0d51c0
Cezar Sas <cezar.sas@gmail.com>
2022-10-10 09:50:03 +0200

    Refactoring of config

--- scripts/R/results_bar.R
+++ scripts/R/results_bar.R
@@ -11,20 +11,20 @@ df$Success.Rate <- df$Success.Rate * 100
 df <- melt(df, variable.name = "metric", 
            value.name = "value")
 
-ggplot(df, aes(x = metric, y = value, color=metric)) +
-  geom_violin(fill = "grey80", show.legend = FALSE) +
-  #  labs(x = 'Similarity', y = "Intersection") +
+ggplot(df, aes(x = metric, y = value, color=metric, fill=metric)) +
+  geom_violin(show.legend = FALSE) +
+  labs(x = element_blank(), y = element_blank()) +
   theme_linedraw() +
   theme(legend.title = element_blank(),
-        legend.text = element_text(size=14),
+        legend.text = element_text(size=17),
         legend.background = element_rect(fill=alpha('white', 0)),
         panel.grid.major.y = element_blank(), #element_line(color = "grey80"),
         panel.grid.major.x = element_blank(), #element_line(color = "grey80"),
         panel.grid.minor.x = element_blank(),
         panel.grid.minor.y = element_blank(),
-        axis.text=element_text(size=14),
-        axis.title=element_text(size=14),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=17),
         axis.text.x = element_text(angle = 45, hjust = 1))
 
-#ggsave("",
-#       width = 8, height = 6, device=cairo_pdf())
+ggsave("results_all.pdf",
+       width = 8, height = 6, device=cairo_pdf())
--- scripts/dataset_stats.py
+++ scripts/dataset_stats.py
@@ -15,14 +15,18 @@ def datastats(cfg: DictConfig):
     """
     lengths = []
     num_labels = Counter()
+    avg_labels = []
     with open(cfg.out_path, "r") as inf:
         for line in inf:
 
             doc = json.loads(line)
+            if len(doc["labels"]) == 0:
+                continue
             readme = doc["readme_text"]
             lengths.append(len(readme.split(' ')))
             print(doc['labels'], doc['topics']) if len(doc['labels']) > 12 else None
             num_labels[len(doc['labels'])] += 1
+            avg_labels.append(len(doc['labels']))
 
     print('Total', len(lengths))
     print('Average', np.mean(lengths))
@@ -30,6 +34,9 @@ def datastats(cfg: DictConfig):
     print('Min', np.min(lengths))
     print('Max', np.max(lengths))
     print(num_labels.most_common())
+    print('Average labels', np.mean(avg_labels))
+    print('STD labels', np.std(avg_labels))
+    print('Annotations', np.sum(avg_labels))
 
 
 if __name__ == '__main__':
